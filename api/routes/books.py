fa901f35e737bafbb7d79a1ae52485671aa5494e
Glenn Tanze <71010568+G4EVA-dev@users.noreply.github.com>
2025-02-12 14:17:25 +0100

    Update books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -68,7 +68,7 @@ async def get_book(book_id: int):
     return book    
 
 
-@router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_id: int) -> None:
-    db.delete_book(book_id)
-    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+# @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_book(book_id: int) -> None:
+#     db.delete_book(book_id)
+#     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
